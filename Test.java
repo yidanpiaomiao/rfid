07b6b52414cee5ff514e930b7180588687e61b3b
unknown <>
2017-02-16 23:41:42 +0800

    dad
    Signed-off-by: unknown <孙建雄>

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class Test{
+	public static void main(String args[]){
+		HashSet<String> hashSet = new HashSet<String>();
+		Set<Sring> set = hashSet;
+	} 
+}
