f642b823ea20b9fdf43bf0ec3d02adf5e3b80309
Wang Weihua <892404475@qq.com>
2017-02-16 23:44:58 +0800

    hahah

--- Test.java
+++ Test.java
@@ -1,6 +1,12 @@
 import java.util.Set;
 import java.util.HashSet;
+-----
+you are a sb
+-----
 
+-----
+21108
+-----
 public class Test{
 	public static void main(String args[]){
 		HashSet<String> hashSet = new HashSet<String>();
