53632e7f7568bf5ea63d48dd8c35998baf1b0559
Wang Weihua <892404475@qq.com>
2017-02-17 09:55:11 +0800

    Test Eclipse Git Plugin

--- Test.java
+++ Test.java
@@ -1,25 +1,12 @@
 import java.util.Set;
 import java.util.HashSet;
------
-you are a sb
------
-
------
-21108
------
+/**
+ * This class is just for testing Git
+ */
 public class Test{
 	public static void main(String args[]){
 		HashSet<String> hashSet = new HashSet<String>();
 		Set<Sring> set = hashSet;
-
-
-
-
-
-
-
-
-
-！!！!！!！!！!！!！!！!！!mabi
+		
 	} 
 }
